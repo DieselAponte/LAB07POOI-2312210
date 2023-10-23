70cbad2c214d73c0526a35b25dee3490695a43c3
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-22 23:45:41 -0500

    Jesus Carmargo

---
+++ Proyecto-Universidad/src/proyecto/universidad/Estudiante.java
@@ -0,0 +1,12 @@
+package proyecto.universidad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estudiante extends Persona {
+    private int codigo;
+    private List<Curso> cursos;
+    private Nota[] notas;
+
+    
+}
