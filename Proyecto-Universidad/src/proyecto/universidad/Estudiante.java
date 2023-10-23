644a5ba172c69ca19ab1bff81b123ee6b92eeb93
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-22 23:47:28 -0500

    Jesus  Camargo

--- Proyecto-Universidad/src/proyecto/universidad/Estudiante.java
+++ Proyecto-Universidad/src/proyecto/universidad/Estudiante.java
@@ -23,5 +23,32 @@ public class Estudiante extends Persona {
         this.codigo = codigo;
     }
 
-    
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public void agregarCurso(Curso curso) {
+        cursos.add(curso);
+    }
+
+    public void asistencia(String registro) {
+        System.out.println("Asistencia del estudiante " + getNombre() + " al curso: " + registro);
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "carnet=" + codigo +
+                ", nombre='" + getNombre() + '\'' +
+                ", apellido='" + getApellido() + '\'' +
+                ", edad=" + getEdad() +
+                ", correo='" + getCorreo() + '\'' +
+                ", telefono=" + getTelefono() +
+                ", cursos=" + cursos +
+                '}';
+    }
 }
