70ad0decf5ae553d47a30002d8ec5c3034e421a8
DarienCbrCode <142682969+DarienCbrCode@users.noreply.github.com>
2023-10-22 22:24:33 -0500

    Darien Cabrera

---
+++ Proyecto-Universidad/src/proyecto/universidad/Universidad.java
@@ -0,0 +1,59 @@
+package proyecto.universidad;
+
+import java.util.Arrays;
+
+public class Universidad {
+    private String nombre;
+    private Sede[] sedes;
+    private Facultad[] facultades;
+    private String rector;
+
+    public Universidad(String nombre, Sede[] sedes, Facultad[] facultades, String rector) {
+        this.nombre = nombre;
+        this.sedes = sedes;
+        this.facultades = facultades;
+        this.rector = rector;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Sede[] getSedes() {
+        return sedes;
+    }
+
+    public void setSedes(Sede[] sedes) {
+        this.sedes = sedes;
+    }
+
+    public Facultad[] getFacultades() {
+        return facultades;
+    }
+
+    public void setFacultades(Facultad[] facultades) {
+        this.facultades = facultades;
+    }
+
+    public String getRector() {
+        return rector;
+    }
+
+    public void setRector(String rector) {
+        this.rector = rector;
+    }
+
+    @Override
+    public String toString() {
+        return "Universidad{" +
+                "nombre='" + nombre + '\'' +
+                ", sedes=" + Arrays.toString(sedes) +
+                ", facultades=" + Arrays.toString(facultades) +
+                ", rector='" + rector + '\'' +
+                '}';
+    }
+}
