56d6a3959c112133a53f45e4288dd3f82508ed79
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-22 23:46:10 -0500

    Jesus Camargo

--- Proyecto-Universidad/src/proyecto/universidad/Estudiante.java
+++ Proyecto-Universidad/src/proyecto/universidad/Estudiante.java
@@ -8,5 +8,20 @@ public class Estudiante extends Persona {
     private List<Curso> cursos;
     private Nota[] notas;
 
+    public Estudiante(int carnet, int dni, String nombre, String apellido, int edad, String correo, int telefono) {
+        super(dni, nombre, apellido, edad, correo, telefono);
+        this.codigo = carnet;
+        this.cursos = new ArrayList<>();
+        this.notas = new Nota[0];
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     
 }
