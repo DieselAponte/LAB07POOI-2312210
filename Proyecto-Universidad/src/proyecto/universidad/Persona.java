8912865b7cfe03be4a08ab91d19d58937d3b23f6
DanielCastroG <142682831+DanielCastroG@users.noreply.github.com>
2023-10-22 23:11:51 -0500

    Add files via upload

---
+++ Proyecto-Universidad/src/proyecto/universidad/Persona.java
@@ -0,0 +1,88 @@
+
+package proyectouniversidad;
+
+public class Persona {
+    private int ID;
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private int telefono;
+    private int codigo;
+
+    public Persona(int dni, String nombre, String apellido, int edad, String correo, int telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void mandarCorreo(String mensaje) {
+        System.out.println("Envio de correo a " + correo + ": " + mensaje);
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni=" + dni +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", correo='" + correo + '\'' +
+                ", telefono=" + telefono +
+                '}';
+    }
+}
+
+
