714945121cb5988d2956ef9be5b9a45216ecf472
AlexanderBtsCode <142683080+AlexanderBtsCode@users.noreply.github.com>
2023-10-22 23:05:27 -0500

    Alexander Benites

--- Proyecto-Universidad/src/proyecto/universidad/Curso.java
+++ Proyecto-Universidad/src/proyecto/universidad/Curso.java
@@ -18,5 +18,63 @@ public class Curso {
         this.horario = horario;
     }
 
-    
+    public String getBloque() {
+        return bloque;
+    }
+
+    public void setBloque(String bloque) {
+        this.bloque = bloque;
+    }
+
+    public String getFormatoDictado() {
+        return formatoDictado;
+    }
+
+    public void setFormatoDictado(String formatoDictado) {
+        this.formatoDictado = formatoDictado;
+    }
+
+    public Persona[] getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(Persona[] personas) {
+        this.personas = personas;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "bloque='" + bloque + '\'' +
+                ", formatoDictado='" + formatoDictado + '\'' +
+                ", personas=" + Arrays.toString(personas) +
+                ", aula='" + aula + '\'' +
+                ", horario=" + horario +
+                ", carrera=" + carrera +
+                '}';
+    }
 }
