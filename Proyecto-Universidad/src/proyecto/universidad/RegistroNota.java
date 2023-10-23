d669ea4a3685051e55d3598566eb558fb6a1ddf7
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-22 23:34:54 -0500

    Diesel Aponte

---
+++ Proyecto-Universidad/src/proyecto/universidad/RegistroNota.java
@@ -0,0 +1,71 @@
+
+package proyecto.universidad;
+
+import java.util.Arrays;
+
+public class RegistroNota {
+    private int id;
+    private Profesor profesor;
+    private Estudiante[] Estudiante;
+    private double notaMinima = 0.0;
+    private double notaMaxima = 20.0;
+    private double notaMinimaAprobacion = 10.5;
+
+    public RegistroNota() {
+    }
+
+    public RegistroNota(int id, Profesor profesor, Estudiante[] Estudiante) {
+        this.id = id;
+        this.profesor = profesor;
+        this.Estudiante = Estudiante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public Estudiante[] getEstudiante() {
+        return Estudiante;
+    }
+
+    public void setEstudiante(Estudiante[] Estudiante) {
+        this.Estudiante = Estudiante;
+    }
+
+    public double getNotaMinima() {
+        return notaMinima;
+    }
+
+    public double getNotaMaxima() {
+        return notaMaxima;
+    }
+
+    public double getNotaMinimaAprobacion() {
+        return notaMinimaAprobacion;
+    }
+
+    @Override
+    public String toString() {
+        long[] estudiante = null;
+        return "RegistroNota{" +
+                "id=" + id +
+                ", profesor=" + profesor +
+                ", estudiante=" + Arrays.toString(estudiante) +
+                ", notaMinima=" + notaMinima +
+                ", notaMaxima=" + notaMaxima +
+                ", notaMinimaAprobacion=" + notaMinimaAprobacion +
+                '}';
+    }
+}
